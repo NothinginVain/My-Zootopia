873f0df6bc2074ddb532e0f6dd6c2c2781437364
José Miguel Ramos <miguelyinyang@icloud.com>
2026-04-23 15:38:22 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -15,18 +15,25 @@ def load_data(file_path):
 animals_data = load_data('animals_data.json')
 
 
+def serialize_animal(animal_obj):
+    out_put = ''
+    out_put += '<li class="cards__item">'
+    out_put += f'<div class="card__title">{animal_obj["name"]}</div>'
+    out_put += '<p class="card__text">'
+    out_put += f'<strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>\n'
+    out_put += f'<strong>Location:</strong> {animal_obj['locations'][0]}<br/>\n'
+    if animal_obj['characteristics'].get('type'):
+        out_put += f'<strong>Type:</strong> {animal_obj['characteristics']['type'].title()}<br/>\n'
+        out_put += '</p>'
+    out_put += '</li>'
+
+    return out_put
+
+
 def get_animal_info():
     out_put = ''
     for animal in animals_data:
-        out_put += '<li class="cards__item">'
-        out_put += f'<div class="card__title">{animal['name']}</div>'
-        out_put += '<p class="card__text">'
-        out_put += f'<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n'
-        out_put += f'<strong>Location:</strong> {animal['locations'][0]}<br/>\n'
-        if animal['characteristics'].get('type'):
-            out_put += f'<strong>Type:</strong> {animal['characteristics']['type'].title()}<br/>\n'
-            out_put += '</p>'
-        out_put += '</li>'
+        out_put += serialize_animal(animal)
     return out_put
 
 filtered_animal_info = get_animal_info()
